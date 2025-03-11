bc165d066c4992d0e803e99342134dc5061ddd08
Emil Pettersson <91emilpettersson@gmail.com>
2025-03-11 15:24:05 +0100

    Add backend unit tests

---
+++ BackEnd/Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Domain.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Domain.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e31ec6e-71dd-4157-98a1-418a8fa36043")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BackEnd/Domain.Test/Services/TheaterServiceTest.cs
@@ -0,0 +1,128 @@
+﻿using AutoFixture;
+using Data.Data;
+using Domain.Services.TheaterService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Test
+{
+    [TestClass]
+    public class TheaterServiceTest
+    {
+        private readonly Fixture fixture;
+        private readonly Mock<ITheaterDataService> theaterDataServiceMock;
+        private readonly TheaterService theaterService;
+
+        public TheaterServiceTest()
+        {
+            fixture = new Fixture();
+
+            theaterDataServiceMock = new Mock<ITheaterDataService>(MockBehavior.Strict);
+
+            theaterService = new TheaterService(theaterDataServiceMock.Object);
+        }
+
+        [TestMethod]
+        public void GetSeatsReturnSeats()
+        {
+            // Arrange 
+            var seatData = fixture.Create<Dictionary<int, bool>>();
+            theaterDataServiceMock.Setup(m => m.GetSeats()).Returns(seatData);
+
+            // Act
+            var result = theaterService.GetSeats();
+
+            // Assert
+            theaterDataServiceMock.Verify(m => m.GetSeats(), Times.Once);
+        }
+
+        [TestMethod]
+        public void TooLargeBookingThrowsException()
+        {
+            // Arrange 
+            var seatNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+            // Act & Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => theaterService.BookSeats(seatNumbers));
+        }
+
+        [TestMethod]
+        public void TooLargeSeatNumberThrowsException()
+        {
+            // Arrange 
+            var seatNumbers = new List<int>() { 0, 1, 2, Common.Constants.NUMBER_OF_SEATS };
+
+            // Act & Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => theaterService.BookSeats(seatNumbers));
+        }
+
+        [TestMethod]
+        public void NegativeSeatNumberThrowsException()
+        {
+            // Arrange 
+            var seatNumbers = new List<int>() { -1, 0, 1, 2 };
+
+            // Act & Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => theaterService.BookSeats(seatNumbers));
+        }
+
+        [TestMethod]
+        public async Task DoesNotDoubleBook()
+        {
+            // Arrange 
+            var seatData = new Dictionary<int, bool>
+            {
+                { 0, false },
+                { 1, true },
+                { 2, true }
+            };
+
+            theaterDataServiceMock.Setup(m => m.GetSeats()).Returns(seatData);
+
+            var seatNumbers = new List<int>() { 0, 1 };
+
+            // Act
+            var result = await theaterService.BookSeats(seatNumbers);
+
+            // Assert
+            Assert.IsNull(result);
+            theaterDataServiceMock.Verify(m => m.Save(It.IsAny<Dictionary<int, bool>>()), Times.Never);
+        }
+
+        [TestMethod]
+        public async Task SuccesfullyBooks()
+        {
+            // Arrange 
+            var seatData = new Dictionary<int, bool>
+            {
+                { 0, true },
+                { 1, true },
+                { 2, true }
+            };
+
+            theaterDataServiceMock.Setup(m => m.GetSeats()).Returns(seatData);
+
+
+            var seatNumbers = new List<int>() { 0, 1 };
+
+            var expected = new Dictionary<int, bool>
+            {
+                { 0, false },
+                { 1, false },
+                { 2, true }
+            };
+
+            theaterDataServiceMock.Setup(m => m.Save(expected)).Returns(expected);
+
+            // Act
+            var result = await theaterService.BookSeats(seatNumbers);
+
+            // Assert
+            Assert.IsNotNull(result);
+            theaterDataServiceMock.Verify(m => m.Save(expected), Times.Once);
+        }
+    }
+}
